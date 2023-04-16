ca03285a667ddb52a7ef113eb6d208e9ede13e8e
csmcgrl <dahapost@gmail.com>
2023-04-16 23:24:32 +0700

    step 2

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-program.version('0.0.1');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .version('0.0.1');
+
 program.parse();
