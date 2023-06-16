2af3822c18a104dbf072c6c2a25127e1bcf2f351
csmcgrl <dahapost@gmail.com>
2023-06-16 11:15:59 +0300

    tests optimization

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -9,42 +9,36 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', `${filename}`);
 
-test('find differences between attached files json', async () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const result = fs.readFileSync(getFixturePath('expected.txt'), 'utf8');
+const stylishResult = fs.readFileSync(getFixturePath('expected.txt'), 'utf8');
+const plainResult = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf8');
+const jsonResult = fs.readFileSync(getFixturePath('expectedJSON.txt'), 'utf8');
+
+const extensions = ['json', 'yml', 'yaml'];
+
+test.each(extensions)('find differences between attached files (%s)', (ext) => {
+  const file1 = getFixturePath(`file1.${ext}`);
+  const file2 = getFixturePath(`file2.${ext}`);
   const difference = gendiff(file1, file2, 'stylish');
-  expect(difference).toEqual(result);
+  expect(difference).toEqual(stylishResult);
 });
 
-test('find differences between attached files yml', async () => {
-  const file1 = getFixturePath('file1.yml');
-  const file2 = getFixturePath('file2.yml');
-  const result = fs.readFileSync(getFixturePath('expected.txt'), 'utf8');
-  const difference = gendiff(file1, file2, 'stylish');
-  expect(difference).toEqual(result);
-});
-
-test('find differences between attached files yaml', async () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yaml');
-  const result = fs.readFileSync(getFixturePath('expected.txt'), 'utf8');
-  const difference = gendiff(file1, file2, 'stylish');
-  expect(difference).toEqual(result);
-});
-
-test('show plain diff', async () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const result = fs.readFileSync(getFixturePath('expectedPlain.txt'), 'utf8');
+test.each(extensions)('show plain diff (%s)', (ext) => {
+  const file1 = getFixturePath(`file1.${ext}`);
+  const file2 = getFixturePath(`file2.${ext}`);
   const difference = gendiff(file1, file2, 'plain');
-  expect(difference).toEqual(result);
+  expect(difference).toEqual(plainResult);
 });
 
-test('show json diff', async () => {
+test.each(extensions)('show json diff (%s)', (ext) => {
+  const file1 = getFixturePath(`file1.${ext}`);
+  const file2 = getFixturePath(`file2.${ext}`);
+  const difference = gendiff(file1, file2, 'json');
+  expect(difference).toEqual(jsonResult);
+});
+
+test('no formatter specified', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
-  const result = fs.readFileSync(getFixturePath('expectedJSON.txt'), 'utf8');
-  const difference = gendiff(file1, file2, 'json');
-  expect(difference).toEqual(result);
+  const difference = gendiff(file1, file2);
+  expect(difference).toEqual(stylishResult);
 });
